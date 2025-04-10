d1a1767dab8d94229fc538a03a8bc0f0055b37d6
Danut <dandragu99@gmail.com>
2025-04-10 08:53:57 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	Tema2/formulario_2/.idea/gradle.xml

--- Tema2/formulario_2/app/src/main/java/com/uax/formulario_2/MainActivity.kt
+++ Tema2/formulario_2/app/src/main/java/com/uax/formulario_2/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.uax.formulario_2
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.uax.formulario_2.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnClickListener {
     // En esta actividad veremos como pulsando un botón en la primera pantalla iremos a la segunda
 
     private lateinit var binding: ActivityMainBinding
@@ -19,5 +22,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         // Hasta aquí ya lo tengo inicializado
 
+        // Queremos asociar pulsaciones a botones ¿Como se hace?
+        binding.btnLogin.setOnClickListener(this)
+        binding.btnLImpiar.setOnClickListener(this)
+
+
+    }
+
+    override fun onClick(v: View?) {
+        // ejecutar cuando un boton o un elemento sea pulsado
+
+        // Lo que tengo que hacer antes de nada es decir que haré si ha sido pulsado
+        // que boton ha sido pulsado?
+        when(v?.id){ // el id del que ha sido pulsado es el id del boton login?
+            binding.btnLogin.id->{ // Si es así haré esto
+                // intent - origen - destino
+                val intent: Intent = Intent(applicationContext)
+            }
+            binding.btnLImpiar.id->{ // Si no es así haré esto otro
+
+            }
+        }
     }
 }
