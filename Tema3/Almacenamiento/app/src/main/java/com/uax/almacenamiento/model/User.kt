b8ca0d05a05683628973418273c80fcb33eb2227
Danut <dandragu99@gmail.com>
2025-04-24 11:41:03 +0200

    añado algún comentario

--- Tema3/Almacenamiento/app/src/main/java/com/uax/almacenamiento/model/User.kt
+++ Tema3/Almacenamiento/app/src/main/java/com/uax/almacenamiento/model/User.kt
@@ -1,10 +1,10 @@
 package com.uax.almacenamiento.model
 
-/*Este es el modelo, esto es con lo que voy a rtbajar
+/*Este es el modelo, esto es con lo que voy a trbajar
 * Cada modelo, necesita una clase asociada a el: Que se va a encargar de hacer transacciones con la
 * base de datos, delete insert upgrade
 *
-* Y para ello voy a crear un DAO- Data Acces Operatio
+* Y para ello voy a crear un DAO- Data Acces Operation
 * Todas las operaciones de la base de datos.
 * */
 
